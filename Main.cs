65dccf74a30262f916ceb1628fde86d783bf6a5a
Dwigth Astacio <dwigth32@outlook.es>
2024-03-30 14:27:46 -0600

    feat(initial): first commit

---
+++ Main.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System.Diagnostics;
+
+public partial class Main : Node
+{
+    // Don't forget to rebuild the project so the editor knows about the new export variable.
+
+    [Export]
+    public PackedScene MobScene { get; set; }
+
+    // We also specified this function name in PascalCase in the editor's connection window
+    private void OnMobTimerTimeout()
+    {
+        // Create a new instance of the Mob scene.
+        Mob mob = MobScene.Instantiate<Mob>();
+
+        // Choose a random location on the SpawnPath.
+        // We store the reference to the SpawnLocation node.
+        var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+        // And give it a random offset.
+        mobSpawnLocation.ProgressRatio = GD.Randf();
+
+        Vector3 playerPosition = GetNode<Player>("Player").Position;
+        mob.Initialize(mobSpawnLocation.Position, playerPosition);
+
+        // Spawn the mob by adding it to the Main scene.
+        AddChild(mob);
+    }
+}
