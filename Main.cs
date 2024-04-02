e00f4b8a337736271e89d66d5a8a87d9deed2e69
Dwigth Astacio <dwigth32@outlook.es>
2024-04-01 20:06:59 -0600

    feat(jump): add jumping and killing enemies

--- Main.cs
+++ Main.cs
@@ -3,27 +3,27 @@ using System.Diagnostics;
 
 public partial class Main : Node
 {
-    // Don't forget to rebuild the project so the editor knows about the new export variable.
+	// Don't forget to rebuild the project so the editor knows about the new export variable.
 
-    [Export]
-    public PackedScene MobScene { get; set; }
+	[Export]
+	public PackedScene MobScene { get; set; }
 
-    // We also specified this function name in PascalCase in the editor's connection window
-    private void OnMobTimerTimeout()
-    {
-        // Create a new instance of the Mob scene.
-        Mob mob = MobScene.Instantiate<Mob>();
+	// We also specified this function name in PascalCase in the editor's connection window
+	private void OnMobTimerTimeout()
+	{
+		// Create a new instance of the Mob scene.
+		Mob mob = MobScene.Instantiate<Mob>();
 
-        // Choose a random location on the SpawnPath.
-        // We store the reference to the SpawnLocation node.
-        var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
-        // And give it a random offset.
-        mobSpawnLocation.ProgressRatio = GD.Randf();
+		// Choose a random location on the SpawnPath.
+		// We store the reference to the SpawnLocation node.
+		var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+		// And give it a random offset.
+		mobSpawnLocation.ProgressRatio = GD.Randf();
 
-        Vector3 playerPosition = GetNode<Player>("Player").Position;
-        mob.Initialize(mobSpawnLocation.Position, playerPosition);
+		Vector3 playerPosition = GetNode<Player>("Player").Position;
+		mob.Initialize(mobSpawnLocation.Position, playerPosition);
 
-        // Spawn the mob by adding it to the Main scene.
-        AddChild(mob);
-    }
+		// Spawn the mob by adding it to the Main scene.
+		AddChild(mob);
+	}
 }
